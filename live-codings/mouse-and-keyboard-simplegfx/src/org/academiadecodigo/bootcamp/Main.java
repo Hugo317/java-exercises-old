cd485d0a14f105c72cc50eec53577da6f8356008
Christina Hinchin <christina.hinchin@academiadecodigo.org>
2023-02-13 10:19:31 +0000

    Added mouse and keyboard handling example

---
+++ live-codings/mouse-and-keyboard-simplegfx/src/org/academiadecodigo/bootcamp/Main.java
@@ -0,0 +1,19 @@
+package org.academiadecodigo.bootcamp;
+
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        //MagicKeyboard magicKeyboard = new MagicKeyboard();
+        //magicKeyboard.init();
+
+        MagicMouse magicMouse = new MagicMouse();
+        magicMouse.init();
+
+    }
+}
