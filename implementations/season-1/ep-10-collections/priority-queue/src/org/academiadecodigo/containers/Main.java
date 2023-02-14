8dbecaf42713129db2fc2ffa72a09de517502746
pedro.jeronimo <pedro.jeronimo@academiadecodigo.org>
2023-02-14 16:21:46 +0000

    directory structure changes

--- implementations/season-1/ep-10-collections/priority-queue/src/org/academiadecodigo/containers/Main.java
+++ implementations/season-1/ep-10-collections/priority-queue/src/org/academiadecodigo/containers/Main.java
@@ -6,12 +6,12 @@ public class Main {
 
         TodoList todoList = new TodoList();
 
-        todoList.add(TodoList.Importance.MEDIUM, 1, "Medium priority 1");
-        todoList.add(TodoList.Importance.LOW, 1, "Low priority 1");
-        todoList.add(TodoList.Importance.HIGH, 1, "High priority 1");
-        todoList.add(TodoList.Importance.LOW, 2, "Low priority 2");
-        todoList.add(TodoList.Importance.MEDIUM, 2, "Medium priority 2");
-        todoList.add(TodoList.Importance.HIGH, 2, "High priority 2");
+        todoList.add(Importance.MEDIUM, 1, "Medium priority 1");
+        todoList.add(Importance.LOW, 1, "Low priority 1");
+        todoList.add(Importance.HIGH, 1, "High priority 1");
+        todoList.add(Importance.LOW, 2, "Low priority 2");
+        todoList.add(Importance.MEDIUM, 2, "Medium priority 2");
+        todoList.add(Importance.HIGH, 2, "High priority 2");
 
         while (!todoList.isEmpty()) {
             System.out.println(todoList.remove());
--- implementations/season-1/ep-10-collections/priority-queue/src/org/academiadecodigo/containers/TodoList.java
+++ implementations/season-1/ep-10-collections/priority-queue/src/org/academiadecodigo/containers/TodoList.java
@@ -4,12 +4,6 @@ import java.util.PriorityQueue;
 
 public class TodoList {
 
-    public enum Importance {
-        HIGH,
-        MEDIUM,
-        LOW
-    }
-
     private PriorityQueue<TodoItem> queue = new PriorityQueue<>();
 
     public void add(Importance importance, int priority, String item) {
@@ -22,38 +16,5 @@ public class TodoList {
 
     public boolean isEmpty() {
         return queue.isEmpty();
-    }
-
-    static public class TodoItem implements Comparable<TodoItem> {
-
-        private Importance importance;
-        private int priority;
-        private String item;
-
-        public TodoItem(Importance importance, int priority, String item) {
-            this.importance = importance;
-            this.priority = priority;
-            this.item = item;
-        }
-
-        @Override
-        public int compareTo(TodoItem todoItem) {
-
-            if (importance.compareTo(todoItem.importance) == 0) {
-                return priority - todoItem.priority;
-            }
-
-            return importance.compareTo(todoItem.importance);
-
-        }
-
-        @Override
-        public String toString() {
-            return "TodoItem{" +
-                    "importance=" + importance +
-                    ", priority=" + priority +
-                    ", item='" + item + '\'' +
-                    '}';
-        }
     }
 }
